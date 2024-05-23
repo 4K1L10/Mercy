222e9a13c75f15643ca4b92c1a178f5785c50174
4K1L10 <akira.paillalef.hidalgo@cftmail.cl>
2024-05-23 00:41:04 -0400

    Agregar archivos de proyecto.

---
+++ Mercy/Models/MercyDeveloperContext.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql.Scaffolding.Internal;
+
+namespace Mercy.Models;
+
+public partial class MercyDeveloperContext : DbContext
+{
+    public MercyDeveloperContext()
+    {
+    }
+
+    public MercyDeveloperContext(DbContextOptions<MercyDeveloperContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Cliente> Clientes { get; set; }
+
+    public virtual DbSet<Servicio> Servicios { get; set; }
+
+    public virtual DbSet<Usuario> Usuarios { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+
+        }
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .UseCollation("utf8_general_ci")
+            .HasCharSet("utf8");
+
+        modelBuilder.Entity<Cliente>(entity =>
+        {
+            entity.HasKey(e => e.IdCliente).HasName("PRIMARY");
+
+            entity.ToTable("cliente");
+
+            entity.Property(e => e.IdCliente)
+                .ValueGeneratedNever()
+                .HasColumnType("int(11)")
+                .HasColumnName("ID_Cliente");
+            entity.Property(e => e.Apellido).HasMaxLength(45);
+            entity.Property(e => e.Correo).HasMaxLength(45);
+            entity.Property(e => e.Direccion).HasMaxLength(45);
+            entity.Property(e => e.Estado).HasMaxLength(45);
+            entity.Property(e => e.Nombre).HasMaxLength(45);
+            entity.Property(e => e.Telefono).HasMaxLength(45);
+        });
+
+        modelBuilder.Entity<Servicio>(entity =>
+        {
+            entity.HasKey(e => e.IdServicio).HasName("PRIMARY");
+
+            entity.ToTable("servicio");
+
+            entity.HasIndex(e => e.IdUsuario, "ID_Usuario_idx");
+
+            entity.Property(e => e.IdServicio)
+                .ValueGeneratedNever()
+                .HasColumnType("int(11)")
+                .HasColumnName("ID_Servicio");
+            entity.Property(e => e.IdUsuario)
+                .HasColumnType("int(11)")
+                .HasColumnName("ID_Usuario");
+            entity.Property(e => e.Nombre).HasMaxLength(100);
+            entity.Property(e => e.Precio).HasColumnType("int(11)");
+            entity.Property(e => e.Sku).HasMaxLength(45);
+
+            entity.HasOne(d => d.IdUsuarioNavigation).WithMany(p => p.Servicios)
+                .HasForeignKey(d => d.IdUsuario)
+                .HasConstraintName("ID_Usuario");
+        });
+
+        modelBuilder.Entity<Usuario>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("usuario");
+
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnType("int(11)")
+                .HasColumnName("ID");
+            entity.Property(e => e.Apellido).HasMaxLength(45);
+            entity.Property(e => e.Correo).HasMaxLength(65);
+            entity.Property(e => e.Nombre).HasMaxLength(45);
+            entity.Property(e => e.Password).HasMaxLength(100);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Mercy/Models/Servicio.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Mercy.Models;
+
+public partial class Servicio
+{
+    public int IdServicio { get; set; }
+
+    public string? Nombre { get; set; }
+
+    public int? Precio { get; set; }
+
+    public string? Sku { get; set; }
+
+    public int? IdUsuario { get; set; }
+
+    public virtual Usuario? IdUsuarioNavigation { get; set; }
+}
